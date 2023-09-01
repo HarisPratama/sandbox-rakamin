36dbe8bf787039c60a533e78ba0c554014ac49e9
haris.pratama <haris.pratama@paxel.co>
2023-09-01 20:16:59 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// console.log(Math.ceil(Math.random() * 50) + 1);
+
+// const arr = []
+
+// math random 
+
+// for (let i = 0; i < 100; i++) {
+//     const randomNumb = Math.round((Math.random() * 50) + 1);
+//     arr.push(randomNumb)
+// }
+
+// console.log(arr);
+
+// const arrOdd = []
+// const arrEven = []
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i, '<<< index');
+//     arrEven.push(i)
+// }
+
+// arrEven.pop()
+
+// console.log(arrEven);
+
+
+const generateRandomNumber = (rangeNum) => {
+    const arr =[]
+
+    for (let i = 0; i < rangeNum; i++) {
+        const randomNumb = Math.round((Math.random() * 50) + 1);
+        arr.push(randomNumb)
+    }
+
+    return arr
+}
+
+
+console.log(generateRandomNumber(50));
