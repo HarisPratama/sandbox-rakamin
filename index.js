750ef861c193dcb0003aeb3e5035bfae00b89644
HarisPratama <hpratama324@gmail.com>
2023-09-08 21:10:36 +0700

    update

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
 
-// console.log(Math.ceil(Math.random() * 50) + 1);
-
-// const arr = []
-
-// math random 
-
-// for (let i = 0; i < 100; i++) {
-//     const randomNumb = Math.round((Math.random() * 50) + 1);
-//     arr.push(randomNumb)
-// }
-
-// console.log(arr);
-
-// const arrOdd = []
-// const arrEven = []
-
-// for (let i = 0; i < 10; i++) {
-//     console.log(i, '<<< index');
-//     arrEven.push(i)
-// }
-
-// arrEven.pop()
-
-// console.log(arrEven);
-
-const generateRandomNumber = (rangeNum) => {
-    const arr =[]
-
-    for (let i = 0; i < rangeNum; i++) {
-        const randomNumb = Math.round((Math.random() * 50) + 1);
-        arr.push(randomNumb)
-    }
-
-    return arr
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return (true)
+  }
+    return (false)
 }
 
-
-console.log(generateRandomNumber(70));
-console.log('hallo');
+console.log(ValidateEmail('haris@mail.com'));
